80b9d43c99a258d5f2ec9530ca78326e77d48d62
benjamint08 <benjamint08@gmail.com>
2024-04-05 15:08:37 +0100

    Add dev logging - thanks @lassejlv for the idea

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import {Glob} from "bun";
 import * as path from "path/posix";
 import * as os from "os";
+import chalk from "chalk";
+
+const isNotProd = process.env.NODE_ENV !== 'production';
 
 interface Route {
     name: string;
@@ -8,9 +11,9 @@ interface Route {
     module: any;
 }
 
-const routes: { [key: string]: Route[] } = {
+const routes: Record<string, Route[]> = {
     get: [],
-    post: [],
+    post: []
 };
 
 async function loadRoutes() {
@@ -36,24 +39,36 @@ async function handleRequest(req: Request): Promise<Response> {
     const start = Date.now();
     const userMethod = req.method.toLowerCase();
     const url = req.url;
+    let isIndex = false;
     let parsedUrl = new URL(url ?? '', 'http://localhost');
+    let reqMessage = `${chalk.bold.white(userMethod.toUpperCase())} ${parsedUrl.pathname}`;
     if(parsedUrl.pathname === '/favicon.ico') {
         return new Response('', { status: 204 });
     }
 
     if(parsedUrl.pathname === '/') {
-        parsedUrl.pathname = '/index';
+        isIndex = true;
     }
 
     let matchingRoute: Route | undefined;
 
     for (const route of routes[userMethod]) {
+        if (isIndex) {
+            if (route.name === 'index') {
+                matchingRoute = route;
+            }
+            continue;
+        }
         if (route.name === parsedUrl.pathname.replace('/', '')) {
             matchingRoute = route;
         }
     }
 
     if (!matchingRoute) {
+        if(isNotProd) {
+            reqMessage += ` ${chalk.bold.red('404')} ${chalk.bold.gray(`${Date.now() - start}ms`)}`;
+            console.log(reqMessage);
+        }
         return new Response('Not found.', { status: 404 });
     }
 
@@ -61,9 +76,31 @@ async function handleRequest(req: Request): Promise<Response> {
         const response = await matchingRoute.module(req);
         const end = Date.now();
         response.headers.set('x-response-time', `${end - start}ms`);
+        if(isNotProd) {
+            let color = 'green';
+            if(response.status >= 100 && response.status < 200) {
+                color = 'blue';
+            } else if(response.status >= 200 && response.status < 300) {
+                color = 'green';
+            } else if(response.status >= 300 && response.status < 400) {
+                color = 'yellow';
+            } else if(response.status >= 400 && response.status < 500) {
+                color = 'purple';
+            } else if(response.status >= 500) {
+                color = 'red';
+            }
+            // @ts-ignore
+            reqMessage += ` ${chalk.bold[color](response.status)}`;
+            reqMessage += ` ${chalk.bold.gray(`${end - start}ms`)}`;
+            console.log(reqMessage);
+        }
         return response;
     } catch (error) {
         console.error('Error while processing the requested route: ', error);
+        if(isNotProd) {
+            reqMessage += ` ${chalk.bold.red('500')} ${chalk.bold.gray(`${Date.now() - start}ms`)}`;
+            console.log(reqMessage);
+        }
         return new Response('Internal Server Error', { status: 500 });
     }
 }
