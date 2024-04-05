7a2b18e3ad90ebab183ca1921c31c62e112775cf
lasse <77295879+lassejlv@users.noreply.github.com>
2024-04-05 18:46:51 +0200

    Added a helper.ts file

---
+++ helper.ts
@@ -0,0 +1,40 @@
+async function SendJSON(json: any, status: number = 200): Promise<Response> {
+    return new Response(JSON.stringify(json), {
+        headers: {
+            "Content-Type": "application/json"
+        },
+        status
+    });
+}
+
+function Success(message: string, status: number = 200): Promise<Response> {
+    return SendJSON({
+        message
+    }, status);
+}
+
+function Failure(message: string, status: number = 400): Promise<Response> {
+    return SendJSON({
+        message
+    }, status);
+}
+
+function Redirect(destination: string, status: number = 302): Response {
+    return new Response(null, {
+        status,
+        headers: {
+            "Location": destination
+        }
+    });
+}
+
+function Html(html: string, status: number = 200): Response {
+    return new Response(html, {
+        headers: {
+            "Content-Type": "text/html"
+        },
+        status
+    });
+}
+
+export { SendJSON, Success, Failure, Redirect, Html };
