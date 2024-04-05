4b3b49b529f7d5c91fdee3845906e329e27c49d6
benjamint08 <benjamint08@gmail.com>
2024-04-05 15:35:39 +0100

    Accidentally forgot to allow posting again

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import {Glob} from "bun";
 import * as path from "path/posix";
 import * as os from "os";
 import chalk from "chalk";
+import * as fs from "fs";
 
 const isNotProd = process.env.NODE_ENV !== 'production';
 
@@ -15,9 +16,9 @@ const get = {} as any;
 const post = {} as any;
 
 async function loadRoutes() {
+    const start = Date.now();
     const allRoutes = new Glob("routes/*.ts");
     for await (let file of allRoutes.scan(".")) {
-        console.log("Loaded route file:", file);
         // Windows handles files differently than Linux
         if(os.platform() === 'win32') {
             file = file.replace(/\\/g, '/');
@@ -35,6 +36,35 @@ async function loadRoutes() {
             post[rawFileName] = postModule;
         }
     }
+    // get all folders and subfolders of cwd
+    for (let file of fs.readdirSync(`${process.cwd()}/routes`)) {
+        if(fs.lstatSync(`${process.cwd()}/routes/${file}`).isDirectory()) {
+            const allRoutes = new Glob(`routes/${file}/*.ts`);
+            for (let file of allRoutes.scanSync(".")) {
+                // Windows handles files differently than Linux
+                if(os.platform() === 'win32') {
+                    file = file.replace(/\\/g, '/');
+                    file = file.replace(/routes\//g, '');
+                }
+                const splits = file.split("/");
+                const rawFileName = splits[splits.length - 1].split(".")[0];
+                const filePath = path.join(process.cwd(), 'routes', file);
+                const routeModule = await import(filePath).then((m) => m.default || m);
+                const getModule = typeof routeModule === 'object' ? routeModule?.GET : routeModule;
+                const postModule = typeof routeModule === 'object' ? routeModule?.POST : routeModule;
+                file = file.replace(/.ts/g, '');
+                if(getModule) {
+                    get[`${file}`] = getModule;
+                }
+                if(postModule) {
+                    post[`${file}`] = postModule;
+                }
+            }
+        }
+    }
+    console.log(get)
+    console.log(post)
+    console.log(`${chalk.bold.white(`Loaded all routes in`)} ${chalk.bold.green(`${Date.now() - start}ms`)}`);
 }
 
 async function handleRequest(req: Request): Promise<Response> {
@@ -59,6 +89,14 @@ async function handleRequest(req: Request): Promise<Response> {
             matchingRoute = get['index'];
         } else {
             matchingRoute = get[parsedUrl.pathname.substring(1)];
+        }
+    }
+
+    if(userMethod === 'post') {
+        if(isIndex) {
+            matchingRoute = post['index'];
+        } else {
+            matchingRoute = post[parsedUrl.pathname.substring(1)];
         }
     }
 
