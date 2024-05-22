820b45cbbca0ae83f3a6c83b278df3e697a6c2bb
benjamint08 <benjamint08@gmail.com>
2024-05-22 20:14:23 +0100

    Fix json to allow stringified json

--- src/helpers/json.ts
+++ src/helpers/json.ts
@@ -3,6 +3,15 @@ export async function json(req: Request): Promise<Object>{
     const readableStream = await req.body.getReader().read();
     const uint8Array = readableStream.value;
     const bodyString = new TextDecoder().decode(uint8Array);
+    if (bodyString === "[object Object]") {
+        return {};
+        // this means that the body is in the form of [object Object]... not right on client end, nothing to do
+        // with ProBun
+    }
+    try {
+        return JSON.parse(bodyString);
+    } catch (e) {
+    }
     let body: any = {};
     bodyString.split("&").forEach((pair) => {
         const [key, value] = pair.split("=");
