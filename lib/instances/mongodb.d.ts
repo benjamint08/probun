403a9140ba537ef92944daa9908cd7ec4ad3ea40
Ben <benjamint08@gmail.com>
2024-05-27 13:17:23 +0100

    Build 0.1.32

--- lib/instances/mongodb.d.ts
+++ lib/instances/mongodb.d.ts
@@ -6,7 +6,7 @@ declare class Mongo {
     getDatabase(db: string): Promise<any>;
     update(db: string, col: string, query: any, update: any): Promise<any>;
     insert(db: string, col: string, data: any): Promise<any>;
-    find(db: string, col: string, query: any): Promise<any[]>;
+    find(db: string, col: string, query: any, options: any): Promise<any[]>;
     findOne(db: string, col: string, query: any): Promise<any>;
     delete(db: string, col: string, query: any): Promise<any>;
     deleteMany(db: string, col: string, query: any): Promise<any>;
